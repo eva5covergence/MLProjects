a7694cd937dd11e442171c55c3aacf937ad3c9d6
Ezhirko <45531125+Ezhirko@users.noreply.github.com>
2024-01-09 21:22:23 +0530

    Initial Commit

---
+++ Student-Performance-Regressor/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="ez",
+    author_email="ezhirko.arulmozhi@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
